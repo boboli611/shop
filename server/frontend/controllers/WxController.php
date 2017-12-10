3d51f879114bb87fca4a2241d5c5f87dd3993e5a
recoba <recoba@cubee.com>
2017-12-10 23:21:20 +0800

    update

--- server/frontend/controllers/WxController.php
+++ server/frontend/controllers/WxController.php
@@ -21,23 +21,26 @@ use \yii\db\Exception as Exception;
  */
 class WxController extends Controller {
     
-    
-    public function actionTest(){
-        var_dump(__LINE__);
-        \frontend\components\WxpayAPI\Pay::pay();
-        echo "aaa";
-    }
-
+   
     public function actionLogin(){
+        
+        /**
+         * 
+         * X-WX-Code:061l48B81dBNxP1WoCy81CzRA81l48Bo
+X-WX-Encrypted-Data:AKQ9R/HnTLUw/etsX+ow1BQWTSqmnLhMJWSkC+kHNIHurxJhap52VpazaP4HBG4AiMHHjgwKVYQ3LXn7RZEBoRiTnEVMlBOogmSruG+x5XsU18tRct1xuNeJJSm++njQ/U88/aTdFRS6fjlGUvVi585jRVKSXwt8isK7ySfIxiiZ6Ehzki32EmRPfVsweGkf9McENF8jNjLY9LITCgW5+aBwbHog44+hPPtznRwCP/JXgl5/mDfduzDTE9WkOjTMY4ooCb1hEkq99hufTWnVBC4OCw3U88HcQAP1f0Ew0dgtuOChwT/WgjzthpOSQTwgpGFSgJqMYVDHVeeWBkKHk0bZcmbUVm4rjksNPI2upawqFwPVMyjjnmrroGlOGlI9xEYGdtzhFZxvqm+hDjaqB+P7a0zCSDuqLO4YGU4Ha5CdVmIdufkoQ9Gv2f2SQNsOn/rd363BJ9m0TFrCjLvVoc3SBfi5eSDkFspyXdgvmg+3B58oZMgOgm8+Hf2aKXGE
+X-WX-IV:wVvxbYA35JXFSshZlrt6Gg==
+         */
  
         $appid = \yii::$app->params["wx"]['appId'];
         $secret = \yii::$app->params["wx"]['appSecret'];
-        $code = "123";
+        $code = $_SERVER['HTTP_X_WX_CODE'];
+        
         $grant_type = "authorization_code";
         $url = "https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$secret&js_code=$code&grant_type=$grant_type";
         $result = widgets\Http::Get($url);
-        $result = '{"openid": "OPENID111","session_key": "SESSIONKEY111","expires_in": 2592000}';
+        //$result = '{"openid": "OPENID111","session_key": "SESSIONKEY111","expires_in": 2592000}';
         $result = json_decode($result, true);
+        var_dump($result);exit;
         if (!$result || isset($result['errcode'])){
             $this->asJson(widgets\Response::error("登录失败"));
             return;
