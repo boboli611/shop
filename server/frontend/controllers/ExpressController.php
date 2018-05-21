09f6dfa3b50de5d7665865a074a62a7aac4f7e8c
recoba <recoba@cubee.com>
2018-05-21 14:50:19 +0800

    no message

--- server/frontend/controllers/ExpressController.php
+++ server/frontend/controllers/ExpressController.php
@@ -16,13 +16,38 @@ use common\widgets;
 class ExpressController extends Controller {
     
     public function actionGet(){
+
         $id =  Yii::$app->request->get("id");
         if (!$id){
             $this->asJson(widgets\Response::error("单号不能为空"));
             return false;
         }
         
-        $info = \common\models\comm\CommExpressLog::find()->where(['no' => $id])->one();
+        $order = \common\models\comm\CommOrder::find()->where(["order_id" => $id])->one();
+        if (!$order){
+            $this->asJson(widgets\Response::error("单号错误"));
+            return false;
+        }
+                echo "aaa";exit;
+        $uid = widgets\User::getUid();
+        if ($order->user_id != $uid){
+            $this->asJson(widgets\Response::error("不是自己订单"));
+            return false;
+        }
+        
+        $orderProduct = \common\models\comm\CommOrderProduct::find()->where(["order_id" => $id])->one();
+        if (!$orderProduct){
+            $this->asJson(widgets\Response::error("订单商品错误"));
+            return false;
+        }
+        
+        $product = \common\models\comm\CommProduct::findOne($orderProduct->product_id);
+        if (!$product){
+            $this->asJson(widgets\Response::error("商品不存在"));
+            return false;
+        }
+        
+        $info = \common\models\comm\CommExpressLog::find()->where(['no' => $order->expressage])->one();
         if (!$info){
             $this->asJson(widgets\Response::error("数据为空"));
             return false;
